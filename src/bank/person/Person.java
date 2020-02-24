0711c13565cd7d82124042863f9ca782588faf41
larsnordstrom <lars.nordstrom@jgy.se>
2020-02-24 19:28:33 +0100

    Created base class for a person

---
+++ src/bank/person/Person.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bank.person;
+/**
+ *
+ * @author Lars Nordstrom <lars.nordstrom@jgy.se>
+ */
+public class Person {
+
+    private String name = "";
+    private String birthdate = "";
+    private String gender = "";
+
+    public Person() {
+
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the birthdate
+     */
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    /**
+     * @param birthdate the birthdate to set
+     */
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        
+        // Hämtar näst sista siffran i p-nummer, konverterar det till heltal och lagrar det i variabelm num.
+        int num = (int) gender.charAt(gender.length() - 2);
+        // Tilldelar ett värde genom att kontrollera om  variabeln num är ojämn. Ojämn = man
+        this.gender = ((num % 2) == 1) ? "man" : "kvinna";
+
+    }
+
+    @Override
+    public String toString() {
+        return "/n/t" + this.getName() + ",/n/t" + this.getGender() + ",/n/t" + this.getBirthdate();
+    }
+
+}
+
