e59d9fdb09ca72fbfae4c27a73e82ee6193ede07
larsnordstrom <lars.nordstrom@jgy.se>
2020-03-30 20:23:46 +0200

    Changes to App for build args.

--- src/bank/App.java
+++ src/bank/App.java
@@ -16,6 +16,8 @@ public class App {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("arg -> " + args[0]);
+        System.out.println("arg -> " + args[1]);
     }
     
 }
